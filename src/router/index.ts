a8fdc3898921207a6828fb518d6d2b735c8a23c3
小逸 <2429016980.com>
2024-09-06 21:49:22 +0800

    feat(auth): 只保留hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,12 @@
 import type { App } from 'vue';
-import {
-  type RouterHistory,
-  createMemoryHistory,
-  createRouter,
-  createWebHashHistory,
-  createWebHistory
-} from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import { createBuiltinVueRoutes } from './routes/builtin';
 import { createRouterGuard } from './guard';
 
-const { VITE_ROUTER_HISTORY_MODE = 'history', VITE_BASE_URL } = import.meta.env;
-
-const historyCreatorMap: Record<Env.RouterHistoryMode, (base?: string) => RouterHistory> = {
-  hash: createWebHashHistory,
-  history: createWebHistory,
-  memory: createMemoryHistory
-};
+const { VITE_BASE_URL } = import.meta.env;
 
 export const router = createRouter({
-  history: historyCreatorMap[VITE_ROUTER_HISTORY_MODE](VITE_BASE_URL),
+  history: createWebHistory(VITE_BASE_URL),
   routes: createBuiltinVueRoutes()
 });
 
